ad1fec631374dbb6385ebf51bf849c5b3a98f659
Zuzike Nemesová <darigjorkinen@gmail.com>
2022-04-19 20:54:58 +0200

    Dodanie tlacitok, uprava fce

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 console.log('funguju');
 
 const zmenaBarvy = () => {
-  const svgElm = document.querySelector('svg');
-  svgElm.style.color = 'white';
+  const svgElm = document.querySelector('#product-image');
+  svgElm.style.fill = 'white';
 };
 
-const bilaKosile = document.querySelector('.bila');
+const bilaKosile = document.querySelector('.white');
 bilaKosile.addEventListener('click', zmenaBarvy);
