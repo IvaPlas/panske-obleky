69f7ee2c2ce89504a54cc7bd6d85d1ba4046c34e
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-04-19 20:21:15 +0200

    js - pridany posluchac na tlacitko bila

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('funguju');
+
+const zmenaBarvy = () => {
+  const svgElm = document.querySelector('svg');
+  svgElm.style.color = 'white';
+};
+
+const bilaKosile = document.querySelector('.bila');
+bilaKosile.addEventListener('click', zmenaBarvy);
